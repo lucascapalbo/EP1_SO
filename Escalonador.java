23d2cb9718050203d33791d999ac3aa3228e7573
GuiCoppini <guilherme.pavani@usp.br>
2017-09-17 13:38:16 -0300

    faz a parada de rodar processo

--- Escalonador.java
+++ Escalonador.java
@@ -5,24 +5,35 @@ import java.util.List;
 
 public class Escalonador {
 
-	private List<Bcp> listaDeProntos = new ArrayList<Bcp>();
-	private List<Bcp> listaDeBloqueado = new ArrayList<Bcp>();
+	private List<Bcp> filaDeProntos = new ArrayList<Bcp>();
+	private List<Bcp> listaDeBloqueados = new ArrayList<Bcp>();
 
 	public void addProcessoPronto(Bcp bcp) {
-		listaDeProntos.add(bcp);
-    ordenaLista();
+		filaDeProntos.add(bcp);
+    ordenaFila();
 	}
 
 	public void addProcessoBloqueado(Bcp bcp) {
-		listaDeBloqueado.add(bcp);
+		listaDeBloqueados.add(bcp);
 	}
 
-  private static void ordenaLista() {
-    Collections.sort(listaDeProntos, new Comparator<Bcp>() {
+  private static void ordenaFila() {
+    Collections.sort(filaDeProntos, new Comparator<Bcp>() {
       @Override
       public int compare(Bcp bcp1, Bcp bcp2) {
         return bcp2.prioridade - bcp1.prioridade;
       }
     });
+  }
+
+
+  public void rodaPrimeiroProcesso() {
+    Bcp bcp = filaDeProntos.get(0); // pega o primeiro da fila
+
+    while(bcp.quantum > 0)
+      // ALGUMA COISA Q RODE A LINHA LÁ
+
+    bcp.prioridade--;
+    ordenaFila():
   }
 }
